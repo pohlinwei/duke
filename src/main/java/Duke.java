1523c9eb7fcffd9da1a4ad4f58591bc8c64dab25
pohlinwei <linwei@Lins-MacBook-Pro-2.local>
2019-08-21 14:17:18 +0800

    Added list functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 public class Duke {
     // for formatting purposes
@@ -16,11 +19,24 @@ public class Duke {
         System.out.print(formatOutput(hi));
 
         String input = "";
+        List<String> tasks = new ArrayList<>();
         // get first user input
         input = sc.nextLine();
 
         while (!input.equals("bye")) {
-            System.out.print(formatOutput(input));
+            if (input.equals("list")) {
+                // print all tasks
+                String listedTask = IntStream.range(0, tasks.size())
+                        .mapToObj(i -> String.format("%d. %s\n", (i + 1), tasks.get(i)))
+                        .reduce("", (prev, curr) -> prev + curr);
+                System.out.print(formatOutput(listedTask));
+
+            } else {
+                System.out.print(formatOutput("added: " + input));
+                // add new task
+                tasks.add(input);
+            }
+            // get new input
             input = sc.nextLine();
         }
 
